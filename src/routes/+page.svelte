65faf7f3c55246986d69dfd0a48b1f6b1ca629dd
raym <raym@users.noreply.github.com>
2023-03-18 11:30:23 -0700

    prompts user, gives help, records numbers, prints ascii

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,87 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { dropLast, equals, ifElse, pipe, prop, test, when } from 'ramda';
+	const ASCII_CODE_FOR_BACKSPACE = 8;
+	$: asciiValue = '';
+	$: decimalValue = '';
+	$: helpText = '';
+	let appendToDecimalValue = (a) => (decimalValue += a);
+	let appendToAsciiValue = (a) => (asciiValue += a);
+	let backspaceDecimalValue = () => (decimalValue = dropLast(1, decimalValue));
+	let backspaceAsciiValue = () => (asciiValue = dropLast(1, asciiValue));
+	let clearDecimalValue = () => (decimalValue = '');
+	let clearHelpText = () => (helpText = '');
+	let convertDecimalValueToAsciiValue = pipe(
+		() => decimalValue,
+		parseInt,
+		ifElse(
+			equals(ASCII_CODE_FOR_BACKSPACE),
+			backspaceAsciiValue,
+			pipe(String.fromCharCode, appendToAsciiValue)
+		),
+		clearDecimalValue
+	);
+	let handleKeydown = pipe(
+		prop('key'),
+		when(test(/\d/), pipe(appendToDecimalValue, clearHelpText)),
+		when(equals('Backspace'), backspaceDecimalValue),
+		when(equals('Enter'), convertDecimalValueToAsciiValue)
+	);
+	let showHelpText = () => (helpText = 'just start typing, this field is disabled');
+</script>
+
+<svelte:window on:keydown={handleKeydown} />
+
+<title>CiiBoard - an ascii keyboard</title>
+<h1>Type the decimal value of an ascii character</h1>
+<h2>Then press enter</h2>
+<label
+	on:click={pipe(clearDecimalValue, showHelpText)}
+	on:keypress={pipe(clearDecimalValue, showHelpText)}
+>
+	<input disabled value={decimalValue} placeholder={helpText} />
+</label>
+<p>
+	For example, type <code>65+enter</code> to print A.
+	<br />
+	65-90 => A-Z and 97-122 => a-z
+</p>
+<div class="ascii-value">
+	{asciiValue}
+</div>
+
+<style>
+	h1 {
+		font-family: 'Andale Mono', sans-serif;
+	}
+
+	h2 {
+		font-family: 'Andale Mono', sans-serif;
+	}
+
+	input {
+		max-width: 500px;
+		width: 100%;
+	}
+
+	input:disabled {
+		pointer-events: none;
+	}
+
+	p {
+		font-family: 'Andale Mono', sans-serif;
+	}
+
+	code {
+		background-color: black;
+		border-radius: 3px;
+		color: white;
+		font-family: 'Andale Mono', sans-serif;
+		padding: 1px 5px;
+	}
+
+	.ascii-value {
+		font-family: 'Andale Mono', sans-serif;
+		font-size: 42px;
+		line-height: 1.5em;
+	}
+</style>
--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,80 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('page has expected prompts', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+
+	await expect(
+		page.getByRole('heading', {
+			name: 'Type the decimal value of an ascii character',
+			level: 1
+		})
+	).toBeVisible();
+
+	await expect(
+		page.getByRole('heading', {
+			name: 'Then press enter',
+			level: 2
+		})
+	).toBeVisible();
+
+	await expect(
+		page.getByText('For example, type 65+enter to print A. 65-90 => A-Z and 97-122 => a-z')
+	).toBeVisible();
+});
+
+test('help text appears when needed (input field is clicked), clears once help is no longer needed', async ({
+	page
+}) => {
+	const EXPECTED_HELP_TEXT = 'just start typing, this field is disabled';
+	const helpText = () => page.getByPlaceholder(EXPECTED_HELP_TEXT, { exact: true });
+	await page.goto('/');
+
+	await expect(page.getByPlaceholder('', { exact: true })).toBeVisible();
+
+	await page.getByPlaceholder('', { exact: true }).click({ force: true });
+	await expect(helpText()).toBeVisible();
+
+	await page.keyboard.type('abc');
+	await expect(helpText()).toBeVisible();
+
+	await page.keyboard.type('123');
+	await expect(helpText()).not.toBeVisible();
+	await expect(page.getByPlaceholder('', { exact: true })).toBeVisible();
+	await expect(page.getByRole('textbox')).toHaveValue('123');
+
+	await page.getByRole('textbox').click({ force: true });
+	await expect(helpText()).toBeVisible();
+	await expect(page.getByRole('textbox')).toHaveValue('');
+});
+
+test('when characters are typed, only numerical characters are added to the input field', async ({
+	page
+}) => {
+	await page.goto('/');
+
+	await expect(page.getByRole('textbox')).toHaveValue('');
+
+	await page.keyboard.type('The meaning of life is 42?!');
+
+	await expect(page.getByRole('textbox')).toHaveValue('42');
+});
+
+test('typing numbers and then pressing Enter will print the ascii value to the page and clear the input', async ({
+	page
+}) => {
+	await page.goto('/');
+
+	// prevent page.keyboard from typing before the page is loaded
+	await expect(page.getByRole('textbox')).toHaveValue('');
+
+	await page.keyboard.type('80');
+	await page.keyboard.press('Enter');
+	await page.keyboard.type('79');
+	await page.keyboard.press('Enter');
+	await page.keyboard.type('87');
+	await page.keyboard.press('Enter');
+	await page.keyboard.type('33');
+	await page.keyboard.press('Enter');
+
+	await expect(page.getByText('POW!')).toBeVisible();
 });
